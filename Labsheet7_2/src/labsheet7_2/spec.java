1088f45297505b757162a3ea7105038d9a81bac0
Ganindu-Deshapriya <153342607+Ganindu-Deshapriya@users.noreply.github.com>
2024-01-29 17:27:39 +0530

    labsheet7 part 2 files

---
+++ Labsheet7_2/src/labsheet7_2/spec.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package labsheet7_2;
+
+/**
+ *
+ * @author ganin
+ */
+enum spec {
+    General, 
+    Cardiothoracic, 
+    Neuro, 
+    Pediatric, 
+    Plastic, 
+    Otolaryngology
+}
