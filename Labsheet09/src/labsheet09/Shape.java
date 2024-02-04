36dca081356e03bf8601593754674243852e8e5b
Ganindu-Deshapriya <153342607+Ganindu-Deshapriya@users.noreply.github.com>
2024-02-04 19:02:02 +0530

    labsheet 9 code added

---
+++ Labsheet09/src/labsheet09/Shape.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labsheet09;
+
+/**
+ *
+ * @author ganin
+ */
+abstract class Shape {
+    public abstract boolean isPolygon();
+    public abstract double calculateCirc();
+    public abstract double calculateArea();
+    
+    
+}
